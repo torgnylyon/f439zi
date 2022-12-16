9f3e79bb0e20c8b645553a866079cce5a978393b
PVVovan <pvvovan@gmail.com>
2022-12-16 16:16:39 +0100

    Set software break points at reset

--- src/startup.c
+++ src/startup.c
@@ -2,10 +2,18 @@
 
 #include <main.h>
 
-void
-reset()
+void __attribute__ ((naked))
+reset(void)
 {
+    asm volatile ("bkpt #0");
+    asm volatile ("ldr sp, =0x2001bf00");
+    asm volatile ("bkpt #1");
+    asm volatile ("ldr sp, =0x2001b000");
+    asm volatile ("bkpt #2");
     main();
+    for ( ; ; ) {
+
+    }
 }
 
 void
@@ -21,11 +29,12 @@ hard_fault()
 }
 
 __attribute__ ((section (".isr_stm"))) uint32_t v[128] = {
-    0x2001bff0,
-    (uint32_t) reset,
+    0x2001bfd0,
+    (uint32_t) &reset,
     (uint32_t) hard_fault,
     (uint32_t) hard_fault,
     (uint32_t) hard_fault,
     (uint32_t) hard_fault,
     (uint32_t) hard_fault,
+    0x0
 };
