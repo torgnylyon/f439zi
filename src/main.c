5f725909d523a48d4c4438ab2d34410d8e773f5f
Torgny Lyon <torgny@abc.se>
2022-12-17 15:50:01 +0100

    Blink green LED

--- src/main.c
+++ src/main.c
@@ -10,21 +10,24 @@ int glov2;
 static int stav1 = 25;
 static int stav2;
 
+static void
+delay(void)
+{
+	for (int i = 0; i < 0x000fffff; ++i)
+		;
+}
+
 int
 main(void)
 {
 	init_clock();
-	asm volatile ("bkpt #0");
 	*(volatile uint32_t *) (GPIOB + GPIO_MODER) |= 1;
-	asm volatile ("bkpt #1");
-	*(volatile uint32_t *) (GPIOB + GPIO_ODR) ^= 1u;
-	asm volatile ("bkpt #2");
-	*(volatile uint32_t *) (GPIOB + GPIO_ODR) ^= 1u;
-	asm volatile ("bkpt #3");
 	while (1) {
 		stav1++;
 		stav2++;
 		glov1++;
 		glov2++;
+		*(volatile uint32_t *) (GPIOB + GPIO_ODR) ^= 1u;
+		delay();
 	}
 }
