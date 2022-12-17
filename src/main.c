e2386c85b9b37695860aee53e2627f4633853b9f
Torgny Lyon <torgny@abc.se>
2022-12-17 15:10:41 +0100

    Fix led toggle bug

--- src/main.c
+++ src/main.c
@@ -15,11 +15,11 @@ main(void)
 {
 	init_clock();
 	asm volatile ("bkpt #0");
-	*((volatile uint32_t *) GPIOB + GPIO_MODER) |= 1;
+	*(volatile uint32_t *) (GPIOB + GPIO_MODER) |= 1;
 	asm volatile ("bkpt #1");
-	*((volatile uint32_t *) GPIOB + GPIO_ODR) ^= 1u;
+	*(volatile uint32_t *) (GPIOB + GPIO_ODR) ^= 1u;
 	asm volatile ("bkpt #2");
-	*((volatile uint32_t *) GPIOB + GPIO_ODR) ^= 1u;
+	*(volatile uint32_t *) (GPIOB + GPIO_ODR) ^= 1u;
 	asm volatile ("bkpt #3");
 	while (1) {
 		stav1++;
