6c0c9923bfc857dbbbc3944184280e2b84e0819b
PVVovan <pvvovan@gmail.com>
2022-12-17 14:45:36 +0100

    Clean up init_clock

--- src/clock.c
+++ src/clock.c
@@ -5,12 +5,10 @@
 #define RCC_AHB1ENR_OFFSET	0x30
 #define RCC_AHB1ENR_GPIOBEN	(1 << 1)
 
-volatile uint32_t *RCC_AHB1ENR = (volatile uint32_t *)(AHB1_RCC_BASE + RCC_AHB1ENR_OFFSET);
+static volatile uint32_t *RCC_AHB1ENR = (volatile uint32_t *)(AHB1_RCC_BASE + RCC_AHB1ENR_OFFSET);
 
 void init_clock(void)
 {
 	/* Enable AHB1 */
 	*RCC_AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
-
-	/* 0x4002 0400 - 0x4002 07FF GPIOB */
 }
