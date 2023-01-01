ac95d9aaa1cfb79327fd46007ff0b24fff38f378
PVVovan <PVVovan@gmail.com>
2023-01-01 14:06:16 +0100

    Replace DMA stream 1 by stream 6

--- src/timdma.c
+++ src/timdma.c
@@ -26,14 +26,14 @@ static const uint32_t DMA_SxCR_PSIZE = 2UL << 11;
 static const uint32_t DMA_SxCR_DIR = 2UL << 6;
 static volatile uint32_t *const DMA2_LIFCR = (volatile uint32_t *const)(DMA2_addr + 0x08);
 static volatile uint32_t *const DMA2_HIFCR = (volatile uint32_t *const)(DMA2_addr + 0x0C);
-static volatile uint32_t *const DMA2_S0CR = (volatile uint32_t *const)(DMA2_addr + 0x10 + 0x18 * 0);
-static volatile uint32_t *const DMA2_S0PAR = (volatile uint32_t *const)(DMA2_addr + 0x18 + 0x18 * 0);
+static volatile uint32_t *const DMA2_S0CR   = (volatile uint32_t *const)(DMA2_addr + 0x10 + 0x18 * 0);
+static volatile uint32_t *const DMA2_S0PAR  = (volatile uint32_t *const)(DMA2_addr + 0x18 + 0x18 * 0);
 static volatile uint32_t *const DMA2_S0MA0R = (volatile uint32_t *const)(DMA2_addr + 0x1C + 0x18 * 0);
 static volatile uint32_t *const DMA2_S0NDTR = (volatile uint32_t *const)(DMA2_addr + 0x14 + 0x18 * 0);
-static volatile uint32_t *const DMA2_S1CR = (volatile uint32_t *const)(DMA2_addr + 0x10 + 0x18 * 1);
-static volatile uint32_t *const DMA2_S1PAR = (volatile uint32_t *const)(DMA2_addr + 0x18 + 0x18 * 1);
-static volatile uint32_t *const DMA2_S1MA0R = (volatile uint32_t *const)(DMA2_addr + 0x1C + 0x18 * 1);
-static volatile uint32_t *const DMA2_S1NDTR = (volatile uint32_t *const)(DMA2_addr + 0x14 + 0x18 * 1);
+static volatile uint32_t *const DMA2_S6CR   = (volatile uint32_t *const)(DMA2_addr + 0x10 + 0x18 * 6);
+static volatile uint32_t *const DMA2_S6PAR  = (volatile uint32_t *const)(DMA2_addr + 0x18 + 0x18 * 6);
+static volatile uint32_t *const DMA2_S6MA0R = (volatile uint32_t *const)(DMA2_addr + 0x1C + 0x18 * 6);
+static volatile uint32_t *const DMA2_S6NDTR = (volatile uint32_t *const)(DMA2_addr + 0x14 + 0x18 * 6);
 static const uint32_t s_gpio_setreg = 1UL << 7;
 static const uint32_t s_gpio_resetreg = 1UL << (7 + 16);
 
@@ -46,23 +46,23 @@ void timdma_setreg(void)
     __sync_synchronize();
     *DMA2_LIFCR = ~0UL;
     *DMA2_HIFCR = ~0UL;
-    *DMA2_S0NDTR = 1;
+    *DMA2_S0NDTR = 1; // Number of data items to transfer
     __sync_synchronize();
     *DMA2_S0CR |= DMA_SxCR_EN; // Enable DMA
 }
 
 void timdma_resetreg(void)
 {
-    *DMA2_S1CR &= ~DMA_SxCR_EN; // Disable DMA
-    while (((*DMA2_S1CR) & DMA_SxCR_EN) != 0) {
+    *DMA2_S6CR &= ~DMA_SxCR_EN; // Disable DMA
+    while (((*DMA2_S6CR) & DMA_SxCR_EN) != 0) {
         ; // wait until DMA is disabled
     }
     __sync_synchronize();
     *DMA2_LIFCR = ~0UL;
     *DMA2_HIFCR = ~0UL;
-    *DMA2_S1NDTR = 1;
+    *DMA2_S6NDTR = 1; // Number of data items to transfer
     __sync_synchronize();
-    *DMA2_S1CR |= DMA_SxCR_EN; // Enable DMA
+    *DMA2_S6CR |= DMA_SxCR_EN; // Enable DMA
 }
 
 void timdma_init(void)
@@ -76,14 +76,14 @@ void timdma_init(void)
     *DMA2_S0CR |= DMA_SxCR_PSIZE; // Peripheral data size word (32-bit)
     *DMA2_S0CR |= DMA_SxCR_DIR; // Data transfer direction Memory-to-memory
 
-    *DMA2_S1MA0R = GPIOB + GPIO_BSRR;
-    *DMA2_S1PAR = (uint32_t)&s_gpio_resetreg;
-    *DMA2_S1CR &= ~DMA_SxCR_CHSEL; // Select channel 0
-    *DMA2_S1CR &= ~DMA_SxCR_PFCTRL; // The DMA is the flow controller
-    *DMA2_S1CR &= ~DMA_SxCR_PL; // Priority level low
-    *DMA2_S1CR |= DMA_SxCR_MSIZE; // Memory data size word (32-bit)
-    *DMA2_S1CR |= DMA_SxCR_PSIZE; // Peripheral data size word (32-bit)
-    *DMA2_S1CR |= DMA_SxCR_DIR; // Data transfer direction Memory-to-memory
+    *DMA2_S6MA0R = GPIOB + GPIO_BSRR;
+    *DMA2_S6PAR = (uint32_t)&s_gpio_resetreg;
+    *DMA2_S6CR &= ~DMA_SxCR_CHSEL; // Select channel 0
+    *DMA2_S6CR &= ~DMA_SxCR_PFCTRL; // The DMA is the flow controller
+    *DMA2_S6CR &= ~DMA_SxCR_PL; // Priority level low
+    *DMA2_S6CR |= DMA_SxCR_MSIZE; // Memory data size word (32-bit)
+    *DMA2_S6CR |= DMA_SxCR_PSIZE; // Peripheral data size word (32-bit)
+    *DMA2_S6CR |= DMA_SxCR_DIR; // Data transfer direction Memory-to-memory
     __sync_synchronize();
 
     *TIM1_SR = 0;
